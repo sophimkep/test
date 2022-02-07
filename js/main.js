2acd60c07b756848593aa7cdcd804569b1932549
sophimkep <89367927+sophimkep@users.noreply.github.com>
2022-02-07 09:16:23 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// get value from input
+
+
+function display(){
+    document.querySelector(".display").style.display = "block"
+    let name = document.querySelector("#name").value;
+    if (name !== "theara" && name !== "theara rit" && name !== "zei ra"){
+        document.querySelector("p").textContent = "Hello my friend " + name + " 😊";
+    }else {
+        document.querySelector("p").textContent = "I love you " + name + " 🥰";
+    }
+}
+
+let submit = document.querySelector("button");
+submit.addEventListener("click",display)
